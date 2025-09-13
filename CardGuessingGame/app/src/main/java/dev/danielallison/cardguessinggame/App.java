d5d3fcfb906554d826143c43e463345e7d929685
Daniel Allison <contact@danielallison.dev>
2025-09-13 17:22:01 +0000

    feat(Domain): Add Domain class, Card class
    Domain class contains business logic for playing cards. Currently includes Card class, with ordered Suit and Rank. Comparisons between suits or between ranks possible, but not yet between cards with suit and rank.

--- CardGuessingGame/app/src/main/java/dev/danielallison/cardguessinggame/App.java
+++ CardGuessingGame/app/src/main/java/dev/danielallison/cardguessinggame/App.java
@@ -8,4 +8,97 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
+
+    /**
+     * The Domain class contains the business logic
+     * In this program, it describes playing cards, their operations, and their order according to this game
+     */    
+    public class Domain {
+        /**
+         * A playing card has a suit and a rank
+         * Playing cards should not be able to change their suit and rank
+         * Static so it can be instantiated without an outer instance
+         */
+        public static class Card {
+            public enum Suit {
+                SPADE(4),
+                HEART(3),
+                DIAMOND(2),
+                CLUB(1);
+
+                private int value;
+
+                /**
+                 * Constructor. In this game, suits have a order, which is represented by an integer
+                 * @param value The integer value of the suit. Spades(4) > Hearts(3) > Diamonds(2) > Clubs(1)
+                 */
+                private Suit(int value) {
+                    this.value = value;
+                }
+
+                /**
+                 * Useful for comparisons
+                 * @return Returns the integer value of the suit
+                 */
+                public int getValue() {
+                    return this.value;
+                }
+            }
+            
+            public enum Rank {
+                TWO(2),
+                THREE(3),
+                FOUR(4),
+                FIVE(5),
+                SIX(6),
+                SEVEN(7),
+                EIGHT(8),
+                NINE(9),
+                TEN(10),
+                JACK(11),
+                QUEEN(12),
+                KING(13),
+                ACE(14);
+                
+                private int value;
+                
+                /**
+                 * Rank constructor. In this game, the rank is represented by an integer
+                 * @param value The integer value of the rank. Aces are high.
+                 */
+                private Rank(int value) {
+                    this.value = value;
+                }
+
+                /**
+                 * Useful for comparisons
+                 * @return Returns the integer value of the rank
+                 */
+                public int getValue() {
+                    return this.value;
+                }
+            }
+
+            private final Rank rank;
+            private final Suit suit;
+
+            /**
+             * Constructor. Creates a new card.
+             * @param suit The suit of the card.
+             * @param rank The rank of the card.
+             */
+            public Card(Suit suit, Rank rank) {
+                this.suit = suit;
+                this.rank = rank;
+            }
+
+            public Suit getSuit() {
+                return this.suit;
+            }
+
+            public Rank getRank() {
+                return this.rank;
+            }
+        }
+    }
 }
--- CardGuessingGame/app/src/test/java/dev/danielallison/cardguessinggame/AppTest.java
+++ CardGuessingGame/app/src/test/java/dev/danielallison/cardguessinggame/AppTest.java
@@ -4,10 +4,31 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import dev.danielallison.cardguessinggame.App;
+import dev.danielallison.cardguessinggame.App.Domain.Card;
 
 public class AppTest {
     @Test public void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+    @Test public void cardSuitOrder() {
+        App app = new App();
+        Card spadeSuit = new Card(Card.Suit.SPADE, Card.Rank.TWO);
+        Card heartSuit = new Card(Card.Suit.HEART, Card.Rank.TWO);
+        Card diamondSuit = new Card(Card.Suit.DIAMOND, Card.Rank.TWO);
+        Card clubSuit = new Card(Card.Suit.CLUB, Card.Rank.TWO);
+
+        assert(spadeSuit.getSuit().getValue() > heartSuit.getSuit().getValue());
+        assert(heartSuit.getSuit().getValue() > diamondSuit.getSuit().getValue());
+        assert(diamondSuit.getSuit().getValue() > clubSuit.getSuit().getValue());
+    }
+
+    @Test public void acesAreHigh() {
+        App app = new App();
+        Card aceCard = new Card(Card.Suit.SPADE, Card.Rank.ACE);
+        Card twoCard = new Card(Card.Suit.SPADE, Card.Rank.TWO);
+
+        assert(aceCard.getRank().getValue() > twoCard.getRank().getValue());
     }
 }
